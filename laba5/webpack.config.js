59be58269d814ab73a8b2902b9cae26ae0fe1706
Anna <losers2004@gmail.com>
2024-05-10 22:47:58 +0200

    Lab5

--- laba5/webpack.config.js
+++ laba5/webpack.config.js
@@ -1,15 +1,36 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const FileManagerPlugin = require('filemanager-webpack-plugin');
 
 module.exports = {
+
+    module: {
+        ruler: [
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+
+
+
     plugins: [
         new HtmlWebpackPlugin({
             template: path.join(__dirname, 'src', 'template.html'),
             filename: 'index.html',
         }),
+        new FileManagerPlugin({
+            events: {
+                onStart: {
+                    delete: ['dist'],
+                },
+            },
+        }),
     ],
-    devServer:{
-        watchFiles: path.join(__dirname,'src'),
+    devServer: {
+        watchFiles: path.join(__dirname, 'src'),
         port: 9000,
     },
 };
