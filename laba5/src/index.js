26caa60841682d9f573b0ed2e53d5c72f0af3a9a
Anna Hnatiuk <losers2004@gmail.com>
2024-05-23 19:10:50 +0200

    creating pages

--- laba5/src/index.js
+++ laba5/src/index.js
@@ -1,9 +1,15 @@
 import './scss/reset.scss'
 import './scss/main.scss'
 import './scss/header.scss'
-import './scss/content.scss'
 import './scss/footer.scss'
 import './scss/font.scss'
+import './scss/template_content.scss'
+import './scss/catalogue_content.scss'
+import './scss/custom_content.scss'
+import './scss/aboutus_content.scss'
+import './scss/liked_content.scss'
+import './scss/profile_content.scss'
+import './scss/cart_content.scss'
 //import './main.js'
 
 // function component(text) {
--- laba5/webpack.config.js
+++ laba5/webpack.config.js
@@ -1,74 +1,100 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path')
+const path = require('path');
 const FileManagerPlugin = require('filemanager-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
+
 module.exports = {
-    entry: path.join(__dirname, 'src', 'index.js'),
+    entry: path.join(__dirname, 'src', 'index.js'),  // Entry point for your application
     output: {
-        path: path.join(__dirname, 'dist'),
-        filename: 'index.[contenthash].js',
-        assetModuleFilename: path.join('images', '[name].[contenthash][ext]'),
+        path: path.join(__dirname, 'dist'),  // Output directory for bundled files
+        filename: 'index.[contenthash].js',  // Output filename with content hash for cache busting
+        assetModuleFilename: path.join('images', '[name].[contenthash][ext]'),  // Output pattern for asset modules
     },
     module: {
         rules: [
             {
-                test: /\.(woff2?|eot|ttf|otf)$/i,
+                test: /\.(woff2?|eot|ttf|otf)$/i,  // Match font files
                 type: 'asset/resource',
                 generator: {
-                    filename: path.join('fonts', '[name].[contenthash][ext]'),
+                    filename: path.join('fonts', '[name].[contenthash][ext]'),  // Output pattern for fonts
                 },
             },
             {
-                test: /\.(png|jpg|jpeg|gif)$/i,
+                test: /\.(png|jpg|jpeg|gif)$/i,  // Match image files
                 type: 'asset/resource',
             },
             {
-                test: /\.svg$/,
+                test: /\.svg$/,  // Match SVG files
                 type: 'asset/resource',
                 generator: {
-                    filename: path.join('icons', '[name].[contenthash][ext]'),
+                    filename: path.join('icons', '[name].[contenthash][ext]'),  // Output pattern for SVG icons
                 },
             },
             {
-                test: /\.js$/,
+                test: /\.js$/,  // Match JavaScript files
                 use: 'babel-loader',
-                exclude: /node-modules/,
+                exclude: /node_modules/,  // Exclude node_modules directory
             },
             {
-                test: /\.pug$/,
-                loader: 'pug-loader'
+                test: /\.pug$/,  // Match Pug template files
+                loader: 'pug-loader',
             },
             {
-                test: /\.(scss|css)$/,
+                test: /\.(scss|css)$/,  // Match SCSS and CSS files
                 use: [
                     MiniCssExtractPlugin.loader,
-                    'css-loader', 'postcss-loader', 'sass-loader'
-                ]
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader',
+                ],
             },
-
-        ]
+        ],
     },
     plugins: [
         new HtmlWebpackPlugin({
-            // template: path.join(__dirname, 'src', 'template.html'),
-            template: path.join(__dirname, 'src', 'template.pug'),
-            filename: 'index.html'
+            // template: path.join(__dirname, 'src', 'template.html'),  // Uncomment if using HTML template
+            template: path.join(__dirname, 'src', 'template.pug'),  // Use Pug template
+            filename: 'index.html',  // Output HTML file name
+        }),
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'src', 'catalogue.pug'),
+            filename: 'catalogue.html'
+        }),
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'src', 'custom.pug'),
+            filename: 'custom.html'
+        }),
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'src', 'aboutus.pug'),
+            filename: 'aboutus.html'
+        }),
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'src', 'liked.pug'),
+            filename: 'liked.html'
+        }),
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'src', 'profile.pug'),
+            filename: 'profile.html'
+        }),
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'src', 'cart.pug'),
+            filename: 'cart.html'
         }),
         new FileManagerPlugin({
             events: {
                 onStart: {
-                    delete: ['dist'],
+                    delete: ['dist'],  // Delete the dist folder before each build
                 },
             },
         }),
         new MiniCssExtractPlugin({
-            filename: '[name].css',
+            filename: '[name].css',  // Output pattern for CSS files
         }),
     ],
     devServer: {
-        watchFiles: path.join(__dirname, 'src'),
-        port: 9000,
+        watchFiles: path.join(__dirname, 'src'),  // Watch for changes in the src directory
+        port: 9000,  // Development server port
     },
     optimization: {
         minimizer: [
